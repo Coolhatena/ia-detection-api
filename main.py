44ae7a83f56c9f092eddfaed868086712871d691
Coolhatena <ingsc.geffroy@gmail.com>
2025-08-16 13:59:26 -0700

    Implement YOLO detecction

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
+from ultralytics import YOLO
 import numpy as np
 import cv2
 from io import BytesIO
 
+model = YOLO("yolov8n.pt").to("cuda")
 app = FastAPI()
 
 @app.post("/process-image/")
@@ -15,16 +17,18 @@ async def process_image(file: UploadFile = File(...)):
 	image_array = np.frombuffer(contents, np.uint8)
 	image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-	# TODO: Process image using YOLO here
-	# Process with OpenCV (example: convert to grayscale)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# Process image using YOLO
+	results = model(image, verbose=False)
+
+	# Draw detections on image
+	annotated_frame = results[0].plot()
 
 	# Get image dimensions as a sample output
-	height, width = gray.shape
+	height, width, _ = annotated_frame.shape
 
 	# Encode image back as original format
 	original_format = '.' + file.content_type.split('/')[1]
-	_, encoded_image = cv2.imencode(original_format, gray)
+	_, encoded_image = cv2.imencode(original_format, annotated_frame)
 
 	# Return result image as a BytesIO buffer
 	return StreamingResponse(
